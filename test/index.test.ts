648a8251e2534671edbfa2e7a962e2e74377217d
Ehsaan Changa <ehsanchanga123@gmail.com>
2025-08-31 13:38:32 +0530

    add test method to check the return sum of numbers in the string

--- test/index.test.ts
+++ test/index.test.ts
@@ -5,4 +5,9 @@ describe("add Numbers", () => {
   test("Add function exists", () => {
     expect(add).toBeDefined();
   });
+
+  it("should return added sum of numbers from the input string", () => {
+    const actual = add("148");
+    expect(actual).toBe(13);
+  });
 });
